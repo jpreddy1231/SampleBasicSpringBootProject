0b4556cb0c96db5ae9593f129ccbf8eb925f1672
jpreddy1231 <jpreddy1231@gmail.com>
2025-10-27 22:09:50 +0530

    intial commit

---
+++ HomeService/src/test/java/com/example/HomeService/HomeServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.HomeService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeServiceApplicationTests {
+
+	/*
+	 * @Test void contextLoads() { }
+	 */
+}
